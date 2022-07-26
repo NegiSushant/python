a5a7849ab582687df637a7bfafce650f8bd022da
Sushant Singh Negi <107835408+NegiSushant@users.noreply.github.com>
2022-07-26 20:46:59 +0530

    Update main.py

--- Day-11 Project (BlackJack Capstone)/main.py
+++ Day-11 Project (BlackJack Capstone)/main.py
@@ -1,10 +1,3 @@
-############### Blackjack Project #####################
-
-#Difficulty Normal 😎: Use all Hints below to complete the project.
-#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
-#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
-#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
-
 ############### Our Blackjack House Rules #####################
 
 ## The deck is unlimited in size.
@@ -18,19 +11,10 @@
 
 ##################### Hints #####################
 
-#Hint 1: Go to this website and try out the Blackjack game:
+#Go to this website and try out the Blackjack game:
 #   https://games.washingtonpost.com/games/blackjack/
-#Then try out the completed Blackjack project here:
-#   http://blackjack-final.appbrewery.repl.run
 
-#Hint 2: Read this breakdown of program requirements:
-#   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
-#Then try to create your own flowchart for the program.
 
-#Hint 3: Download and read this flow chart I've created:
-#   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
-
-#Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
 #11 is the Ace.
 import random
 import clear
